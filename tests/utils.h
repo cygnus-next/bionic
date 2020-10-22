24cb3e693e036f692004432c20a60accc305c6be
Elliott Hughes <enh@google.com>
2020-10-22 13:22:35 -0700

    Add DoNotOptimize and use it in tests.
    Bug: http://b/148307629
    Test: treehugger
    Change-Id: I3b1726ae55116f6553ea38fe163abdde179c21f0
    Signed-off-by: Akash <akashniki@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- tests/utils.h
+++ tests/utils.h
@@ -278,3 +278,13 @@ class FdLeakChecker {
 
   size_t start_count_ = CountOpenFds();
 };
+
+// From <benchmark/benchmark.h>.
+template <class Tp>
+static inline void DoNotOptimize(Tp const& value) {
+  asm volatile("" : : "r,m"(value) : "memory");
+}
+template <class Tp>
+static inline void DoNotOptimize(Tp& value) {
+  asm volatile("" : "+r,m"(value) : : "memory");
+}
